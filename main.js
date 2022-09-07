f66ee84425578ed47d082a7c0a22bd70c12f5d95
77iu <112532158+77iu@users.noreply.github.com>
2022-09-06 21:44:54 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,50 @@
 var z = document.getElementById("z");
-var show02 = document.getElementById("show01");
+var show01 = document.getElementById("show01");
 var show02 = document.getElementById("show02");
-var show02 = document.getElementById("show02");
+var show03 = document.getElementById("show03");
+var show04 = document.getElementById("show04");
+var show05 = document.getElementById("show05");
+var show06 = document.getElementById("show06");
+var show07 = document.getElementById("show07");
+var show08 = document.getElementById("show08");
+var show09 = document.getElementById("show09");
+var show10 = document.getElementById("show10");
+var show11 = document.getElementById("show11");
+var show12 = document.getElementById("show12");
+var show13 = document.getElementById("show13");
+var show14 = document.getElementById("show14");
+var show15 = document.getElementById("show15");
+var show16 = document.getElementById("show16");
+var show17 = document.getElementById("show17");
+var show18 = document.getElementById("show18");
+var show19 = document.getElementById("show19");
+var show20 = document.getElementById("show20");
+var show21 = document.getElementById("show21");
+var show22 = document.getElementById("show22");
+var show23 = document.getElementById("show23");
+var show24 = document.getElementById("show24");
+var show25 = document.getElementById("show25");
+var show26 = document.getElementById("show26");
+var show27 = document.getElementById("show27");
+var show28 = document.getElementById("show28");
+var show29 = document.getElementById("show29");
+var show30 = document.getElementById("show30");
+var show31 = document.getElementById("show31");
+var show32 = document.getElementById("show32");
+var show33 = document.getElementById("show33");
+var show34 = document.getElementById("show34");
+var show35 = document.getElementById("show35");
+var show36 = document.getElementById("show36");
+var show37 = document.getElementById("show37");
+var show38 = document.getElementById("show38");
+var show39 = document.getElementById("show39");
+var show40 = document.getElementById("show40");
+var show41 = document.getElementById("show41");
+var show42 = document.getElementById("show42");
+var show43 = document.getElementById("show43");
+var show44 = document.getElementById("show44");
+var show45 = document.getElementById("show45");
+var show46 = document.getElementById("show46");
 var modal = document.getElementById("myImg");
 var detalle = document.getElementById("caption");
 
@@ -178,7 +221,59 @@ show36.onclick = function(){
     z.style.display ="block";
     modal.src = this.src;
     detalle.innerHTML = this.alt
-                                    }                                                                                                                                                     
+                                    }  
+show37.onclick = function(){
+    z.style.display ="block";
+    modal.src = this.src;
+    detalle.innerHTML = this.alt
+                                    } 
+show38.onclick = function(){
+    z.style.display ="block";
+    modal.src = this.src;
+    detalle.innerHTML = this.alt
+                                    } 
+show39.onclick = function(){
+    z.style.display ="block";
+    modal.src = this.src;
+    detalle.innerHTML = this.alt
+                                    } 
+show40.onclick = function(){
+    z.style.display ="block";
+    modal.src = this.src;
+    detalle.innerHTML = this.alt
+                                    }  
+show41.onclick = function(){
+    z.style.display ="block";
+    modal.src = this.src;
+    detalle.innerHTML = this.alt
+                                    } 
+show42.onclick = function(){
+    z.style.display ="block";
+    modal.src = this.src;
+    detalle.innerHTML = this.alt
+                                    } 
+show43.onclick = function(){
+    z.style.display ="block";
+    modal.src = this.src;
+    detalle.innerHTML = this.alt
+                                    } 
+show44.onclick = function(){
+    z.style.display ="block";
+    modal.src = this.src;
+    detalle.innerHTML = this.alt
+                                    }  
+show45.onclick = function(){
+    z.style.display ="block";
+    modal.src = this.src;
+    detalle.innerHTML = this.alt
+                                    } 
+show46.onclick = function(){
+    z.style.display ="block";
+    modal.src = this.src;
+    detalle.innerHTML = this.alt
+                                    }                                                                                                                 
+                                   
+                                                                                                                                                                                          
                                                                       
         // var span = document.getElementsByClassName("close")[0];
         var span =document.getElementById("myImg");
